4ee5dc12059b38dd09c25413af4d12ea9cddf4ff
VitaliyPitsa <vitalik.gorshkov.1913@mail.ru>
2022-03-09 22:15:55 +0300

    test1

---
+++ test1/ConsoleApplication3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int z, x, c, v;
+    cout<< "Hello World!\n";
+    z = 1;
+    x = 2;
+    c = 3;
+    v = z + x - c;
+    count << v << endl;
+}
+
